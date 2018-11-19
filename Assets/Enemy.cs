2689a1ba26befc61607777281eec8ba7004e6e09
jaroslawrutk <choresize@gmail.com>
2018-11-20 00:54:00 +0100

    Initial commit

---
+++ Assets/Enemy.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+    private Transform Target;
+    private int wavepointIndex = 0;
+    private void Start()
+    {
+        Target = Waypoints.points[0];
+    }
+    private void Update()
+    {
+        Vector3 direction = Target.position - transform.position;
+        transform.Translate(direction.normalized *speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, Target.position) <= 2.0f)
+            GetNextWaypoint();
+    }
+    void GetNextWaypoint ()
+    {
+        if(wavepointIndex>=Waypoints.points.Length-1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+        Target = Waypoints.points[wavepointIndex];
+    }
+}
---
+++ Assets/dzialaj.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class dzialaj : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+    private int WaveIndex = 0;
+    public Text wavecountdowntext;
+
+
+    private void Update()
+    {
+        if (countdown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+
+        wavecountdowntext.text = Mathf.Floor(countdown).ToString();
+
+    }
+    IEnumerator SpawnWave()
+    {
+
+        WaveIndex++;
+        for (int i = 0; i < WaveIndex; i++)
+        {
+
+            SpwanEnemy();
+            yield return new WaitForSeconds(0.5f);
+            Debug.Log("NADCHODZA ALGOSY");
+        }
+
+    }
+    void SpwanEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+
+}
